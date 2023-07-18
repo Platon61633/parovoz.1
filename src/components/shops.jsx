368597101b4cef04032f1d0c4061004e61044a78
Platon61633 <peredreevplaton@gmail.com>
2023-07-18 17:16:11 +0300

    make a css of footer

--- src/components/shops.jsx
+++ src/components/shops.jsx
@@ -4,11 +4,11 @@ const Shops = () => {
     return(
         <div className='shops' id='shops'>
             <div>
-                <p>Наши магазины:</p>
-                <p>1. Магазин на ул. Площадь Восстания 1-2</p>
-                <p>2. Магазин на Гоголевском пер. 10 (1 этаж)</p>
+                <span>Наши магазины:</span><br/>
+                <span>1. Магазин на ул. Площадь Восстания 1-2</span><br />
+                <span>2. Магазин на Гоголевском пер. 10 (1 этаж)</span>
             </div>
-            <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A11482dd91bd3ab1ce18165623c1b35cd2d54e311784bff9117cad63378e61c3a&amp;source=constructor" width="60%" height="400" frameborder="0"></iframe>
+            <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A11482dd91bd3ab1ce18165623c1b35cd2d54e311784bff9117cad63378e61c3a&amp;source=constructor" width="800" height="400" frameborder="0"></iframe>
         </div>
     );
 };
